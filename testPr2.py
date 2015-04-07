a13008a0fe454146fbe3289aa8f40de9e9b5d08a
Tomas Lozano-Perez <tlp@mit.edu>
2015-04-07 08:36:04 -0400

    Make test1 for robot

--- testPr2.py
+++ testPr2.py
@@ -612,20 +612,20 @@ def test1(hpn = True, skeleton = False, hierarchical = False, heuristic=habbs,
     varDict = {} if easy else {'table1': (0.07**2, 0.03**2, 1e-10, 0.2**2),
                                'table2': (0.07**2, 0.03**2, 1e-10, 0.2**2),
                                'objA': (0.1**2, 0.1**2, 1e-10, 0.3**2)} 
-    varDict = {} if easy else {'table1': (0.03**2, 0.03**2, 1e-10, 0.05**2),
-                               'table2': (0.03**2, 0.03**2, 1e-10, 0.05**2),
-                               'objA': (0.05**2, 0.05**2, 1e-10, 0.05**2)} 
+    # varDict = {} if easy else {'table1': (0.03**2, 0.03**2, 1e-10, 0.05**2),
+    #                            'table2': (0.03**2, 0.03**2, 1e-10, 0.05**2),
+    #                            'objA': (0.05**2, 0.05**2, 1e-10, 0.05**2)} 
     front = util.Pose(1.1, 0.0, tZ, 0.0)
     table2Pose = util.Pose(1.0, -1.00, 0.0, 0.0)
     t = PlanTest('test1',  errProbs, allOperators,
                  objects=['table1', 'objA'],
                  movePoses={'objA': front})
 
-    region = 'table2Left'
+    region = 'table1Left'
     goal = State([Bd([In(['objA', region]), True, goalProb], True)])
 
     t = PlanTest('test1',  errProbs, allOperators,
-                 objects=['table1', 'objA', 'table2'],
+                 objects=['table1', 'objA'],
                  fixPoses={'table1': util.Pose(1.3, 0.0, 0.0, math.pi/2),
                            'table2': table2Pose},
                  movePoses={'objA': front},
