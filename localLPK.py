942e49e903d10f1c59da2260051674d7fa565627
Leslie P Kaelbling <lpk@mit.edu>
2015-08-17 22:39:13 -0400

    various fixes, change obs on move

--- localLPK.py
+++ localLPK.py
@@ -104,10 +104,10 @@ debugOnly = ['h', 'assign']  # print but don't pause
 #------------------------------------
 # Add tags that you want to debug and pause on to this list
 
-debugOn = usualTags + ['executionSurprise', 'executionFail', 'getShadowWorld']
+debugOn = usualTags + ['executionSurprise', 'executionFail']
 #+ ['disablePickPlace'] 
 debugOnly = debugOnly
-traceOnly = traceOnly + ['nextStep']
+traceOnly = traceOnly 
                                                  
 
 
